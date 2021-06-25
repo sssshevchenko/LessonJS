05a329570a5e3fc56f1081efc13249377f6d83eb
sssshevchenko <denshev233@gmail.com>
2021-06-25 19:19:16 +0300

    23,25-06

--- 2021-06-23,25/js/script.js
+++ 2021-06-23,25/js/script.js
@@ -1,60 +1,78 @@
+// @ts-nocheck
 const formAddTask = document.querySelector('#form-add-task');
 const taskList = document.querySelector('#task-list');
 const taskListContainer = document.querySelector('#task-list-container');
 
-formAddTask.addEventListener('submit', function(event){
-  event.preventDefault();
+formAddTask.addEventListener('submit', function(event) {
+	event.preventDefault();
 
-  //console.log(this.children.title.value);   //title - name of input
-  const task = {
-    title: this.children.title.value,
-    done: false,
-    id: new Date().getTime()
-  };
+	const task = {
+		title: this.children.title.value, // input value
+		done: false,
+		id: new Date().getTime()
+	};
 
-  addTask(task);
-  
-  localStorage.setItem(String(task.id), JSON.stringify(task));
+	addTask(task);
 
-  this.reset();
+	localStorage.setItem(String(task.id), JSON.stringify(task));
+
+	markContainer();
+	countTasks();
+
+	this.reset();
 });
 
-function addTask(task){
-  const listItem = document.createElement('li');
-  listItem.innerText = task.title;
-  listItem.classList.add('list-item');
-  listItem.setAttribute('data-id', task.id);
-  taskList.appendChild(listItem);
-  //listItem.innerHTML = '<button class="btn-close">&times;</button>'
-  const btnDelete = document.createElement('button');
-  btnDelete.classList.add('btn-close');
-  btnDelete.innerHTML = '&times;';
-  listItem.appendChild(btnDelete);
+// read data
+for (let key in localStorage) {
+	if (localStorage.hasOwnProperty(key)) {
+		const task = JSON.parse(localStorage[key]);
+		addTask(task);
+	}
 }
 
-function markContainer(){
-  if(localStorage.length){
-    taskListContainer.classList.add('has-tickets');
-  } else{
-    taskListContainer.classList.remove('has-tickets');
-  }
-}
+taskList.addEventListener('click', event => {
+	if (!event.target.classList.contains('btn-close')) return;
+
+	// const parentNode = event.target.parentNode;
+	const { parentNode } = event.target; // <li>
+
+	const taskId = parentNode.getAttribute('data-id');
+	localStorage.removeItem(taskId);
+	parentNode.remove();
+
+	markContainer();
+	countTasks();
+});
 
 markContainer();
-//read data from localStorage
-for(let key in localStorage){
-  if(localStorage.hasOwnProperty(key)){
-    const task = JSON.parse(localStorage[key]);
-    addTask(task);
-  };
+countTasks();
+
+function addTask(task) {
+	const listItem = document.createElement('li');
+	listItem.innerText = task.title;
+	listItem.classList.add('list-item');
+	listItem.setAttribute('data-id', task.id);
+
+	const btnDelete = document.createElement('button');
+	btnDelete.classList.add('btn-close');
+	btnDelete.innerHTML = '&times;';
+
+	taskList.appendChild(listItem);
+	listItem.appendChild(btnDelete);
 }
 
-taskList.addEventListener('click', (event) =>{
-  if(!event.target.classList.contains('btn-close')) return;
+function markContainer() {
+	if (localStorage.length) {
+		taskListContainer.classList.add('has-tickets');
+	} else {
+		taskListContainer.classList.remove('has-tickets');
+	}
+}
 
-  //const parentNode = event.target.parentNode;
-  const {parentNode} = event.target;
-  const taskId = parentNode.getAttribute('data-id');
-  localStorage.removeItem(taskId);
-  parentNode.remove();
-})
+function countTasks() {
+	const element = document.querySelector('#task-count');
+
+	const values = Object.values(localStorage);
+
+	element.innerText = values.filter(item => !JSON.parse(item).done).length;
+}
